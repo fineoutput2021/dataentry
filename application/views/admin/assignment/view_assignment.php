7420fff7101c04419510e8fa3875c6834c3564ff
manjeeta choudhary <manjee562@gmail.com>
2022-12-01 11:59:37 +0530

    page added

---
+++ application/views/admin/assignment/view_assignment.php
@@ -0,0 +1,195 @@
+<div class="content-wrapper">
+<section class="content-header">
+<h1>
+  Assignment
+
+  </ol>
+</h1>
+
+
+</section>
+<section class="content">
+<div class="row">
+<div class="col-lg-12">
+<a class="btn btn-info cticket" href="<?php echo base_url() ?>dcadmin/assignment/add_assignment" role="button" style="margin-bottom:12px;"> Add Assignment</a>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title"><i class="fa fa-money fa-fw"></i>View Assignment</h3>
+            </div>
+               <div class="panel panel-default">
+
+               			  <? if(!empty($this->session->flashdata('smessage'))){ ?>
+               			        <div class="alert alert-success alert-dismissible">
+               			    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+               			    <h4><i class="icon fa fa-check"></i> Alert!</h4>
+               			  <? echo $this->session->flashdata('smessage'); ?>
+               			  </div>
+               			    <? }
+               			     if(!empty($this->session->flashdata('emessage'))){ ?>
+               			     <div class="alert alert-danger alert-dismissible">
+               			  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+               			  <h4><i class="icon fa fa-ban"></i> Alert!</h4>
+               			<? echo $this->session->flashdata('emessage'); ?>
+               			</div>
+               			  <? } ?>
+
+
+            <div class="panel-body">
+                <div class="box-body table-responsive no-padding">
+                <table class="table table-bordered table-hover table-striped" id="assignmentTable">
+
+
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>student</th>
+                            <th>digit</th>
+                            <th>Assignment</th>
+                            <th>Deadline Date</th>
+                            <th>Word</th>
+                            <th>Total amount</th>
+                            <th>Paid Amount</th>
+                            <th>Pending Amount</th>
+                            <th>Document</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                              </tr>
+                          </thead>
+                          <tbody>
+
+                        	<?php $i=1; foreach($assignment_data->result() as $data) {
+                            $numlength = strlen((string)$data->digit);
+                            if($numlength==1){
+                              $digit="000".$data->digit;
+                            }else if($numlength==2){
+                                $digit="00".$data->digit;
+                            }else if($numlength==3){
+                                $digit="0".$data->digit;
+                            }else{
+                                $digit=$data->digit;
+                            }
+                            ?>
+  <tr>
+      <td><?php echo $i ?> </td>
+      <td><?
+$id=$data->student;
+$this->db->select('*');
+            $this->db->from('tbl_student');
+            $this->db->where('id',$id);
+            $dsa= $this->db->get();
+            $da=$dsa->row();
+            if(!empty($da))
+            {
+              echo $da->full_name;
+
+           }
+
+?>
+</td>
+
+      <td><?php echo $digit ?></td>
+      <td><?php echo $data->assignment_name ?></td>
+
+      <td><?php echo $data->deadline_date ?></td>
+      <td><?php echo $data->word_count ?></td>
+      <td> ₹<?php echo $data->total_amount ?></td>
+      <td> ₹<?php echo $data->paid_amount ?></td>
+      <td> ₹<?php echo $data->pending_amount ?></td>
+      <td>
+                <?php if($data->image!=""){  ?>
+  <img id="slide_img_path" height=50 width=100  src="<?php echo base_url()."".$data->image ?>" >
+            <?php }else {  ?>
+            Sorry No image Found
+            <?php } ?>
+              </td>
+
+
+                  <td><?php if($data->is_active==0){ ?>
+              <p class="label bg-yellow" >pending</p>
+
+              <?php } else { ?>
+              <p class="label bg-yellow" >complete</p>
+
+              <?php		}   ?>
+              </td>
+              <td>
+              <div class="btn-group" id="btns<?php echo $i ?>">
+              <div class="btn-group">
+              <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false"> Action <span class="caret"></span></button>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="<?php echo base_url() ?>dcadmin/assignment/update_image/<?php echo base64_encode($data->id) ?>">Complete Assignment</a></li>
+
+              <li><a href="<?php echo base_url() ?>dcadmin/assignment/update_assignment/<?php echo base64_encode($data->id) ?>">Edit</a></li>
+
+              <!-- <a class="" href="<?php echo base_url() ?>dcadmin/assignment/update_image" role="button" style="margin-bottom:12px;"> Add image</a> -->
+
+              <li><a href="javascript:;" class="dCnf" mydata="<?php echo $i ?>">Delete</a></li>
+              </ul>
+              </div>
+              </div>
+
+<div style="display:none" id="cnfbox<?php echo $i ?>">
+<p> Are you sure delete this </p>
+<a href="<?php echo base_url() ?>dcadmin/assignment/delete_assignment/<?php echo base64_encode($data->id); ?>" class="btn btn-danger" >Yes</a>
+<a href="javasript:;" class="cans btn btn-default" mydatas="<?php echo $i ?>" >No</a>
+</div>
+</td>
+</tr>
+<?php $i++; } ?>
+</tbody>
+</table>
+
+
+
+
+
+
+                </div>
+            </div>
+        </div>
+
+        </div>
+
+    </div>
+    </div>
+</section>
+</div>
+
+
+<style>
+label{
+margin:5px;
+}
+</style>
+<script src="<?php echo base_url() ?>assets/admin/plugins/datatables/jquery.dataTables.js"></script>
+<script src="<?php echo base_url() ?>assets/admin/plugins/datatables/dataTables.bootstrap.js"></script>
+<script type="text/javascript">
+
+$(document).ready(function(){
+$('#assignmentTable').DataTable({
+responsive: true,
+// bSort: true
+});
+
+$(document.body).on('click', '.dCnf', function() {
+var i=$(this).attr("mydata");
+console.log(i);
+
+$("#btns"+i).hide();
+$("#cnfbox"+i).show();
+
+});
+
+$(document.body).on('click', '.cans', function() {
+var i=$(this).attr("mydatas");
+console.log(i);
+
+$("#btns"+i).show();
+$("#cnfbox"+i).hide();
+})
+
+});
+
+</script>
+<!-- <script type="text/javascript" src="<?php echo base_url() ?>assets/slider/ajaxupload.3.5.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets/slider/rs.js"></script>	  -->
