ed3568a084bac9e689d25d20de433d208a2516f8
manjeeta choudhary <manjee562@gmail.com>
2022-12-02 11:40:18 +0530

    update students

--- application/views/admin/student/update_student.php
+++ application/views/admin/student/update_student.php
@@ -1,7 +1,7 @@
 <div class="content-wrapper">
   <section class="content-header">
     <h1>
-      Add New Students
+      Update Students
     </h1>
     <ol class="breadcrumb">
       <li><a href="<?php echo base_url() ?>admin/dashboard"><i class="fa fa-dashboard"></i> Home</a></li>
@@ -15,7 +15,7 @@
 
         <div class="panel panel-default">
           <div class="panel-heading">
-            <h3 class="panel-title"><i class="fa fa-money fa-fw"></i> Add Student </h3>
+            <h3 class="panel-title"><i class="fa fa-money fa-fw"></i> Update Student </h3>
           </div>
 
           <? if (!empty($this->session->flashdata('smessage'))) { ?>
